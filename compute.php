bca66235da121f2a3a9dff15b1e9f02d1bfe27fd
Mae Ann Pacquiao <142951976+Ann0405@users.noreply.github.com>
2023-08-30 22:57:26 +0800

    Update compute.php

--- compute.php
+++ compute.php
@@ -9,8 +9,6 @@
   <body>
   	<div class="container-fluid">
 
-
-
    <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">MY SHOP</a>
